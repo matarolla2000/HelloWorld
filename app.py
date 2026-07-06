f88902d82e7dae7a0906a434580cf79ceb58aa64
matarolla2000 <matarolla2000@gmail.com>
2026-07-06 12:51:27 +0500

    app.py index.html login.html

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     pass
+
+@app.route("/login", methods="POST")
+def login():
+    data = request.form
+    login = data['login']
+    resp = make_response(render_template('index.html', login=login))
+    resp.set_cookie("login", login)
+    return render_template("login.html", login=login)
